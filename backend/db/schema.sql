a6a6a8eede5c2d76adc644346ce183bcfe6a7a53
JustKarth <karthikkonala14@gmail.com>
2026-01-17 17:05:48 +0530

    Version 2 of the schema.sql and the markdown with the database's pseudo-code has been added

--- backend/db/schema.sql
+++ backend/db/schema.sql
@@ -1,6 +1,11 @@
 CREATE DATABASE IF NOT EXISTS campus_care;
 USE campus_care;
 
+CREATE TABLE avatars (
+	avatar_id INT AUTO_INCREMENT PRIMARY KEY,
+    avatar_url VARCHAR(2048) NOT NULL
+);
+
 CREATE TABLE states (
 	state_id INT AUTO_INCREMENT PRIMARY KEY,
     state_name VARCHAR(50) NOT NULL
@@ -15,6 +20,14 @@ CREATE TABLE colleges (
     FOREIGN KEY (state_id) REFERENCES states(state_id) ON UPDATE CASCADE ON DELETE RESTRICT
 );
 
+CREATE TABLE courses (
+	course_id INT AUTO_INCREMENT PRIMARY KEY,
+    college_id INT NOT NULL,
+    course_name VARCHAR(128) NOT NULL,
+    UNIQUE KEY(course_id, college_id),
+    FOREIGN KEY (college_id) REFERENCES colleges(college_id)
+);
+
 CREATE TABLE local_guide_categories(
 	category_id INT AUTO_INCREMENT PRIMARY KEY,
     category_name VARCHAR(100) NOT NULL
@@ -24,31 +37,50 @@ CREATE TABLE user_profiles (
 	user_id INT AUTO_INCREMENT PRIMARY KEY,
     is_moderator BOOLEAN DEFAULT 0,
     is_admin BOOLEAN DEFAULT 0,
-    reg_no INT NOT NULL,
+    reg_no VARCHAR(50) NOT NULL,
     email VARCHAR(254) NOT NULL,
     hashed_password VARCHAR(255) NOT NULL,
+    avatar_id INT DEFAULT 1,
     date_of_birth DATE NOT NULL,
     first_name VARCHAR(50) NOT NULL,
     middle_name VARCHAR(50),
     last_name VARCHAR(50),
     college_id INT NOT NULL,
+    course_id INT NOT NULL,
     graduation_year INT NOT NULL,
     native_state_id INT,
     native_city VARCHAR(50),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (course_id, college_id) REFERENCES courses(course_id, college_id),
+    FOREIGN KEY (avatar_id) REFERENCES avatars(avatar_id),
     FOREIGN KEY (college_id) REFERENCES colleges(college_id),
     FOREIGN KEY (native_state_id) REFERENCES states(state_id) ON UPDATE CASCADE ON DELETE SET NULL
+);
+
+CREATE TABLE blog_images (
+	blog_image_id INT AUTO_INCREMENT PRIMARY KEY,
+    blog_image_url VARCHAR(2048) NOT NULL
 );
 
 CREATE TABLE blog (
 	blog_id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT NOT NULL,
     college_id INT NOT NULL,
-    blog_title VARCHAR(255) NOT NULL,
+    blog_title VARCHAR(128) NOT NULL,
     blog_content TEXT NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES user_profiles(user_id) ON DELETE CASCADE,
     FOREIGN KEY (college_id) REFERENCES colleges(college_id)
+);
+
+CREATE TABLE blog_specific_images (
+	blog_id INT NOT NULL,
+    blog_image_id INT NOT NULL,
+    image_index TINYINT NOT NULL,
+    PRIMARY KEY(blog_id, blog_image_id),
+    FOREIGN KEY (blog_id) REFERENCES blog(blog_id) ON DELETE CASCADE,
+    FOREIGN KEY (blog_image_id) REFERENCES blog_images(blog_image_id) ON DELETE CASCADE,
+    UNIQUE(blog_id, image_index)
 );
 
 CREATE TABLE blog_comments (
@@ -85,7 +117,7 @@ CREATE TABLE places (
     place_name VARCHAR(255) NOT NULL,
     place_description TEXT,
     address VARCHAR(255),
-    distance FLOAT,
+    distance DECIMAL(6, 2),
     website VARCHAR(2048),
     phone VARCHAR(20),
     FOREIGN KEY (category_id) REFERENCES local_guide_categories(category_id) ON UPDATE CASCADE ON DELETE CASCADE,
