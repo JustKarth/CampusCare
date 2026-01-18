4a5c9f9de2c8454b8e7ab365ec49cd963e995e38
JustKarth <karthikkonala14@gmail.com>
2026-01-19 01:00:44 +0530

    Added first version of initial data for databases, some init data is yet to be written. Added a db_queries markdown to further add all the code for queries required by the team. Will be done after the next team meet.

---
+++ backend/db/init_data.sql
@@ -0,0 +1,66 @@
+USE campus_care;
+
+/*
+INSERT INTO avatars (avatar_url) VALUES 
+('<path>');
+
+INSERT INTO local_guide_categories (category_name) VALUES
+('<category_name>');
+
+INSERT INTO academic_resources (college_id, resource_title, resource_description, resource_link) VALUES
+(1, '<title>', '<description>', '<link>');
+
+INSERT INTO places (category_id, college_id, place_name, place_description, address, distance, website, phone) VALUES
+(<category_id>, 1, <name>, <description>, <address>, <distance>, <website>, <phone>);
+*/ 
+
+INSERT INTO states (state_name) VALUES
+('Andhra Pradesh'),
+('Arunachal Pradesh'),
+('Assam'),
+('Bihar'),
+('Chattisgarh'),
+('Goa'),
+('Gujrat'),
+('Haryana'),
+('Himachal Pradesh'),
+('Jharkhand'),
+('Karnataka'),
+('Kerala'),
+('Madhya Pradesh'),
+('Maharashtra'),
+('Manipur'),
+('Meghalaya'),
+('Mizoram'),
+('Nagaland'),
+('Odisha'),
+('Punjab'),
+('Rajasthan'),
+('Sikkim'),
+('Tamil Nadu'),
+('Telangana'),
+('Tripura'),
+('Uttar Pradesh'),
+('Uttarakhand'),
+('West Bengal'),
+('UT : Andaman and Nicobar Islands'),
+('UT : Chandigarh'),
+('UT : Dadra and Nagar Haveli and Daman and Diu'),
+('UT : Delhi(NCT)'),
+('UT : Jammu and Kashmir'),
+('UT : Ladakh'),
+('UT : Lakshadweep'),
+('UT : Puducherry');
+
+INSERT INTO colleges (college_id, email_domain, college_name, city, state_id) VALUES (1, '@mnnit.ac.in', 'Motilal Nehru National Institute of Technology Allahabad', 'Prayagraj', 26);
+
+INSERT INTO courses (college_id, course_name) VALUES
+(1, 'B.Tech. in Computer Science and Engineering'),
+(1, 'B.Tech. in Electronics and Communication Engineering'),
+(1, 'B.Tech. in Electrical Engineering'),
+(1, 'B.Tech. in Mechanical Engineering'),
+(1, 'B.Tech. in Chemical Engineering'),
+(1, 'B.Tech. in Civil Engineering'),
+(1, 'B.Tech. in Bio Technology'),
+(1, 'B.Tech. in Materials Engineering'),
+(1, 'B.Tech. in Production and Industrial Engineering');
